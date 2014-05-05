d6b35572699108e6b01db8003d7a4518542098e4
idxco <idxco@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-05-05 17:16:30 +0000

    Updated plugin version to 1.1.3. Added creating dynamic page feature.
    git-svn-id: https://plugins.svn.wordpress.org/idx-broker-platinum/trunk@908356 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- js/idx-buttons.js
+++ js/idx-buttons.js
@@ -4,7 +4,7 @@
 			// Register commands
 			ed.addCommand('mcebutton', function() {
 				ed.windowManager.open({
-					file : url + 'button-popup.php', // file that contains HTML for our modal window
+					file : url + '/../button-popup.php', // file that contains HTML for our modal window
 					width : 620 + parseInt(ed.getLang('button.delta_width', 0)), // size of our window
 					height : 340 + parseInt(ed.getLang('button.delta_height', 0)), // size of our window
 					inline : 1
@@ -13,7 +13,7 @@
 				});
 			});
 			// Register buttons
-			ed.addButton('idx_button', {title : 'IDX', cmd : 'mcebutton', image: url + 'images/icon.png' });
+			ed.addButton('idx_button', {title : 'IDX', cmd : 'mcebutton', image: url + '/../images/icon.png' });
 		},
 		getInfo : function() {
 			return {
@@ -28,3 +28,4 @@
 	// second parameter must match the first parameter of the tinymce.create() function above
 	tinymce.PluginManager.add('idx_button', tinymce.plugins.shortcodePlugin);
 })();
+
